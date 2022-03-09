63a950de3b3fe8219a45377126feb766f702f5a4
soumik9876 <soumik9876@gmail.com>
2022-03-09 23:09:54 +0600

    add article and add quiz dialogs designed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  reactStrictMode: false,
-  presets: [
-    "next/babel"
-  ],
-  plugins: [
-    "babel-plugin-macros",
-    "babel-plugin-polished",
-    "@vanilla-extract/babel-plugin"
-  ]
+    reactStrictMode: false,
+    presets: [
+        "next/babel"
+    ],
+    plugins: [
+        "babel-plugin-macros",
+        "babel-plugin-polished",
+        "@vanilla-extract/babel-plugin"
+    ],
+    images: {
+        domains: ["*"],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
 }
