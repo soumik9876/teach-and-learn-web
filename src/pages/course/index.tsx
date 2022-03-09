d265da89d44742d2e19ee04e5d320e7272112259
Saurav-Paul <sauravpaul.sunny@gmail.com>
2022-03-09 15:03:49 +0600

    banner added on course page

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React from "react";
 
 export default function IndividualCourse() {
-  return (
-	<div className="w-screen h-screen bg-c_background">
-		hhiii</div>
-  )
+	const Banner = () => {
+		return (
+			<div className='w-full flex bg-c_primary_light rounded-[22px]'>
+				<div className='flex-[.7] p-20'>
+					<div>
+						<div>
+							<span
+								className='font-ralewayl'
+								style={{
+									fontStyle: "normal",
+									fontWeight: "bold",
+									fontSize: "36px",
+									lineHeight: "42px",
+								}}
+							>
+								{`"Online learning is not the next big thing, it is the now big thing.” - Donna J. Abernathy`}
+							</span>
+						</div>
+						<div className='pt-12 flex space-x-4'>
+							<button className='bg-c_primary_main rounded-md px-2 py-1'>
+								<span
+									style={{
+										fontStyle: "normal",
+										fontSize: "14px",
+										lineHeight: "21px",
+									}}
+								>
+									Your Courses
+								</span>
+							</button>
+							<button>
+								<span
+									style={{
+										fontFamily: "Raleway",
+										fontStyle: "normal",
+										fontWeight: "bold",
+										fontSize: "14px",
+										lineHeight: "21px",
+										textDecorationLine: "underline",
+										color: "#000000",
+									}}
+								>
+									Teach
+								</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className='flex-1 flex justify-center items-center'>
+					<img src='./banner.png' alt='' />
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<div className='w-screen h-screen bg-c_background flex justify-center py-20'>
+			<div className='w-[80%]'>
+				<Banner />
+			</div>
+		</div>
+	);
 }
