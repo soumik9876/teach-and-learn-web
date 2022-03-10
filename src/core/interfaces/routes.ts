839eff53fe754ccbf3c6c759f41aa8392de07543
Saurav-Paul <sauravpaul.sunny@gmail.com>
2022-03-10 23:55:17 +0600

    retrieve api link added

--- src/core/interfaces/routes.ts
+++ src/core/interfaces/routes.ts
@@ -1,36 +1,36 @@
 export const BASE_API_URL = "https://teach-and-learn-api.herokuapp.com/api/";
 
-
 export const REST_API_ENDPOINTS = {
-    auth: {
-        v1: {
-            login: BASE_API_URL + "accounts/v1/login/google/"
-        }
-    },
-    course: {
-        v1: {
-            course: BASE_API_URL + "course/v1/course/",
-            category: BASE_API_URL+"course/v1/course_category/",
-            video: BASE_API_URL+ "course/v1/video/"
-        }
-    }
-}
+	auth: {
+		v1: {
+			login: BASE_API_URL + "accounts/v1/login/google/",
+		},
+	},
+	course: {
+		v1: {
+			course: BASE_API_URL + "course/v1/course/",
+			retrieve_course: (id: any) => BASE_API_URL + `course/v1/course/${id}`,
+			category: BASE_API_URL + "course/v1/course_category/",
+			video: BASE_API_URL + "course/v1/video/",
+		},
+	},
+};
 
 export const ROUTES = {
-    courses: "/courses",
-    createCourse: "/create-course",
-    about: "/about",
-    home: "/",
-    course: (id: any = undefined) => {
-        return id ? `/course/${id}` : "/course/";
-    },
-    article: (id: any = undefined) => {
-        return id ? `/article/${id}` : "/article/";
-    },
-    video: (id: any = undefined) => {
-        return id ? `/video/${id}` : "/video/";
-    },
-    quiz: (id: any = undefined) => {
-        return id ? `/quiz/${id}` : "/quiz/";
-    },
+	courses: "/courses",
+	createCourse: "/create-course",
+	about: "/about",
+	home: "/",
+	course: (id: any = undefined) => {
+		return id ? `/course/${id}` : "/course/";
+	},
+	article: (id: any = undefined) => {
+		return id ? `/article/${id}` : "/article/";
+	},
+	video: (id: any = undefined) => {
+		return id ? `/video/${id}` : "/video/";
+	},
+	quiz: (id: any = undefined) => {
+		return id ? `/quiz/${id}` : "/quiz/";
+	},
 };
