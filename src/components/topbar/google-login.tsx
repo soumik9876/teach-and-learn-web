666d737cddd12bec64a6f6c9d17e326b31f3eeb2
soumik9876 <soumik9876@gmail.com>
2022-03-10 15:11:58 +0600

    google login implemented

--- src/components/topbar/google-login.tsx
+++ src/components/topbar/google-login.tsx
@@ -4,20 +4,46 @@ import theme from "../../site-settings/material-ui-theme/theme";
 import GoogleLogin from "react-google-login";
 import {debug_print} from "../../core/utils";
 import {useState} from "react";
+import {googleLoginSuccess, serverLoginSuccess} from "../../redux/auth/action";
+import {useDispatch} from "react-redux";
+import {useRouter} from "next/router";
+import {REST_API_ENDPOINTS, ROUTES} from "../../core/interfaces/routes";
 
 const GoogleLoginButton = () => {
     const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const serverLogin = async (access_token: string) => {
+        const response = await fetch(REST_API_ENDPOINTS.auth.v1.login, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({access_token}),
+        });
+        if (!response.ok) {
+            // TODO snackbar to be added
+            debug_print("server login failed");
+            return;
+        }
+        const data = await response.json();
 
-    const onGoogleLoginSuccess = (response:any) => {
+        dispatch(serverLoginSuccess(data));
+
+        router.push(router.pathname).then();
+        debug_print(data);
+    };
+    const onGoogleLoginSuccess = (response: any) => {
         debug_print(response);
         setOpen(true);
-        // dispatch(googleLoginSuccess(response.tokenObj))
-        //
-        // serverLogin(response.accessToken).then();
+        dispatch(googleLoginSuccess(response.tokenObj))
+        serverLogin(response.accessToken).then();
     };
     const onFailureCallback = () => {
 
     }
+    debug_print(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID)
     return (
         <>
             <GoogleLogin
@@ -25,11 +51,13 @@ const GoogleLoginButton = () => {
                 buttonText="Login"
                 onSuccess={onGoogleLoginSuccess}
                 onFailure={onFailureCallback}
-                render={renderProps => (
+                render={(renderProps) => (
                     <Button startIcon={<GoogleIcon/>} variant={"contained"} sx={{
                         bgcolor: `${theme.palette.primary.main}!important`,
-                        color: `${theme.palette.text.primary}!important`
-                    }}>Login</Button>
+                        color: `${theme.palette.text.primary}!important`,
+
+                    }} onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}>Login</Button>
                 )}
                 // redirectUri={"http://localhost:3000/login/"}
                 // cookiePolicy={'single_host_origin'}
--- src/core/interfaces/routes.ts
+++ src/core/interfaces/routes.ts
@@ -1,25 +1,29 @@
-export const BASE_API_URL = "https://teach-and-learn-api.herokuapp.com/api";
+export const BASE_API_URL = "https://teach-and-learn-api.herokuapp.com/api/";
 
 
-export const API_ROUTES = {
-
+export const REST_API_ENDPOINTS = {
+    auth: {
+        v1: {
+            login: BASE_API_URL + "accounts/v1/login/google/"
+        }
+    },
 }
 
 export const ROUTES = {
-	courses: "/courses",
-	createCourse: "/create-course",
-	about: "/about",
-	home: "/",
-	course: (id: any = undefined) => {
-		return id ? `/course/${id}` : "/course/";
-	},
-	article: (id: any = undefined) => {
-		return id ? `/article/${id}` : "/article/";
-	},
-	video: (id: any = undefined) => {
-		return id ? `/video/${id}` : "/video/";
-	},
-	quiz: (id: any = undefined) => {
-		return id ? `/quiz/${id}` : "/quiz/";
-	},
+    courses: "/courses",
+    createCourse: "/create-course",
+    about: "/about",
+    home: "/",
+    course: (id: any = undefined) => {
+        return id ? `/course/${id}` : "/course/";
+    },
+    article: (id: any = undefined) => {
+        return id ? `/article/${id}` : "/article/";
+    },
+    video: (id: any = undefined) => {
+        return id ? `/video/${id}` : "/video/";
+    },
+    quiz: (id: any = undefined) => {
+        return id ? `/quiz/${id}` : "/quiz/";
+    },
 };
